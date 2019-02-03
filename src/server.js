f42b1b0a66e936a662e1cd0ec11316dd7528e028
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-02-04 00:26:31 +0500

    Done middleware and router work

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,4 +13,14 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+router.get('/me', (req, res) => {
+  res.send({ me: 'hello' })
+})
+
+app.use('/api', router)
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
